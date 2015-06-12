6b20370b4dcd2dd2c5e1cf9c3dfcd1b23927702f
Syrus Mesdaghi <syrusm@gmail.com>
2015-06-12 16:03:25 -0400

    basic service working

--- tilebundler/tilebundler/views.py
+++ tilebundler/tilebundler/views.py
@@ -1,9 +1,14 @@
+import os
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.http import HttpResponse
+from django.views.static import serve
+import json
 
-from tilebundler.models import Tileset
+from .models import Tileset
+import time
 
 
 class IndexView(generic.ListView):
@@ -11,6 +16,30 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Tileset.objects.order_by('-created_at')[:5]
+
+
+#TODO: use as part of index view
+def json_view_all(request):
+    objects = []
+    for obj in Tileset.objects.all():
+        objects.append(obj.to_minimal_dict())
+    return HttpResponse(json.dumps(objects), content_type='json')
+
+
+def json_view(request, tileset_id):
+    tileset = get_object_or_404(Tileset, pk=tileset_id)
+    return HttpResponse(tileset.to_json(), content_type='json')
+
+
+#TODO: serve through nginx instead
+#TODO: use as part of detail view
+def download_view(request, tileset_id):
+    tileset = get_object_or_404(Tileset, pk=tileset_id)
+    file_path = '{}/{}.{}'.format('./cache_data', tileset.name, 'mbtiles')
+    file_path = os.path.abspath(file_path)
+    response = serve(request, os.path.basename(file_path), os.path.dirname(file_path))
+    response['Content-Disposition'] = 'attachment; filename="{}"'.format(os.path.basename(file_path))
+    return response
 
 
 class DetailView(generic.DetailView):
@@ -26,9 +55,10 @@ class ResultsView(generic.DetailView):
 def generate(request, tileset_id):
     tileset = get_object_or_404(Tileset, pk=tileset_id)
     try:
-        #selected_name = request.POST['name']
-        #tileset.save()
         print 'generate(), tileset: ', tileset
+        print 'created_at: ', int(time.mktime(tileset.created_at.timetuple())*1000)
+        print 'created_by: ', tileset.created_by
+        tileset.generate_tileset()
     except Exception:
         return render(request, 'tilesets/detail.html', {
             'tileset': tileset,
--- tilebundler/tilebundler_prj/urls.py
+++ tilebundler/tilebundler_prj/urls.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^tilesets/', include('tilebundler.urls', namespace="tilesets")),
+    #url(r'^tilesets.json', include('tilebundler.urls', namespace='tilesets.json')),
+    url(r'^tilesets/', include('tilebundler.urls', namespace='tilesets')),
     url(r'^admin/', include(admin.site.urls)),
+    #url(r'^json/', include('tilebundler.urls', namespace='tilesets')),
 )
